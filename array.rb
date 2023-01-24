3a02a90c52407f406c59d663904676a2f78322d3
gotandajidou <116865648+gotandajidou@users.noreply.github.com>
2023-01-24 22:39:07 +0900

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,64 @@
+Array.new               #=> []
+Array.new(3)            #=> [nil, nil, nil]
+Array.new(3, 7)         #=> [7, 7, 7]
+Array.new(3, true)      #=> [true, true, true]
+
+str_array = ["This", "is", "a", "small", "array"]
+
+str_array[0]            #=> "This"
+str_array[1]            #=> "is"
+str_array[4]            #=> "array"
+str_array[-1]           #=> "array"
+str_array[-2]           #=> "small"
+
+str_array = ["This", "is", "a", "small", "array"]
+
+str_array.first         #=> "This"
+str_array.first(2)      #=> ["This", "is"]
+str_array.last(2)       #=> ["small", "array"]
+
+#Adding an element to an existing array is as simple as using the #push method or the shovel operator <<. Both methods will add elements to the end of an array and return that array with the new elements. The #pop method will remove the element at the end of an array and return the element that was removed.
+num_array = [1, 2]
+
+num_array.push(3, 4)      #=> [1, 2, 3, 4]
+num_array << 5            #=> [1, 2, 3, 4, 5]
+num_array.pop             #=> 5
+num_array                 #=> [1, 2, 3, 4]
+
+#The methods #shift and #unshift are used to add and remove elements at the beginning of an array. The #unshift method adds elements to the beginning of an array and returns that array (much like #push). The #shift method removes the first element of an array and returns that element (much like #pop).
+num_array = [2, 3, 4]
+
+num_array.unshift(1)      #=> [1, 2, 3, 4]
+num_array.shift           #=> 1
+num_array                 #=> [2, 3, 4]
+
+num_array = [1, 2, 3, 4, 5, 6]
+
+num_array.pop(3)          #=> [4, 5, 6]
+num_array.shift(2)        #=> [1, 2]
+num_array                 #=> [3]
+
+
+a = [1, 2, 3]
+b = [3, 4, 5]
+a + b         #=> [1, 2, 3, 3, 4, 5]
+a.concat(b)   #=> [1, 2, 3, 3, 4, 5]
+
+[1, 1, 1, 2, 2, 3, 4] - [1, 4]  #=> [2, 2, 3]
+
+[].empty?               #=> true
+[[]].empty?             #=> false
+[1, 2].empty?           #=> false
+
+[1, 2, 3].length        #=> 3
+
+[1, 2, 3].reverse       #=> [3, 2, 1]
+
+[1, 2, 3].include?(3)   #=> true
+[1, 2, 3].include?("3") #=> false
+
+[1, 2, 3].join          #=> "123"
+[1, 2, 3].join("-")     #=> "1-2-3"
+
+
+
