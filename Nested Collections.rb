5d438dd200c16925b1cf6bd3d0c7008117db000d
gotandajidou <116865648+gotandajidou@users.noreply.github.com>
2023-01-25 00:50:06 +0900

    Create Nested Collections.rb

---
+++ Nested Collections.rb
@@ -0,0 +1,19 @@
+teacher_mailboxes = [
+  ["Adams", "Baker", "Clark", "Davis"],
+  ["Jones", "Lewis", "Lopez", "Moore"],
+  ["Perez", "Scott", "Smith", "Young"]
+]
+
+teacher_mailboxes[0][0]
+#=> "Adams"
+teacher_mailboxes[1][0]
+#=> "Jones"
+teacher_mailboxes[2][0]
+#=> "Perez"
+
+teacher_mailboxes[0][-1]
+#=> "Davis"
+teacher_mailboxes[-1][0]
+#=> "Perez"
+teacher_mailboxes[-1][-2]
+#=> "Smith"
